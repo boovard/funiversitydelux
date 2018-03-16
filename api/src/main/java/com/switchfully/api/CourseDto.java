7410d4d7c1c9461012e03ecf2099643520ca8e35
reneald <delea.vanreeth@gmail.com>
2018-03-16 09:59:46 +0100

    implemented CourseDto (for now)

--- api/src/main/java/com/switchfully/api/CourseDto.java
+++ api/src/main/java/com/switchfully/api/CourseDto.java
@@ -1,9 +1,59 @@
 package com.switchfully.api;
 
 public class CourseDto {
+    //TODO create separate Dto for toDomain and toDto so that incoming can provide professorId and outgoing provides a professorDto
     private int courseId;
     private String name;
     private double studyPoints;
     private String category;
     private int professorId;
+
+    public static CourseDto courseDto() {
+        return new CourseDto();
+    }
+
+    public CourseDto withCourseId(int courseId) {
+        this.courseId = courseId;
+        return this;
+    }
+
+    public CourseDto withName(String name){
+        this.name = name;
+        return this;
+    }
+
+    public CourseDto withStudyPoints(int studyPoints){
+        this.studyPoints = studyPoints;
+        return this;
+    }
+
+    public CourseDto withCategory(String category) {
+        this.category = category;
+        return this;
+    }
+
+    public CourseDto withProfessorId(int professorId) {
+        this.professorId = professorId;
+        return this;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getStudyPoints() {
+        return studyPoints;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
 }
